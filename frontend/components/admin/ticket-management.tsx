bf6d05fa084349aa5fb0f286f1369217885c201d
Ziya Rashid <144579482+ziyarashidd@users.noreply.github.com>
2025-11-09 15:52:50 +0530

    Delete frontend directory

--- frontend/components/admin/ticket-management.tsx
+++
@@ -1,9 +0,0 @@
-import AdminTickets from "./admin-tickets"
-
-interface TicketManagementProps {
-  tickets: any[]
-}
-
-export default function TicketManagement({ tickets }: TicketManagementProps) {
-  return <AdminTickets tickets={tickets} />
-}
